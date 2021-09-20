c0d52cdcdc716f6763aa72263d8907f769d7397f
mirandora <info@mirandora.com>
2021-09-20 20:44:20 +0900

    Add files via upload

---
+++ 7_1/ARCharacterController.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_PlacedPrefab;
+
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    public GameObject spawnedObject { get; private set; }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    ARRaycastManager m_RaycastManager;
+
+    bool objectsetFlag = false;
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (objectsetFlag)
+        {
+            Vector3 tmp_pos = spawnedObject.transform.position;
+            tmp_pos.z += 0.01f;
+
+            spawnedObject.transform.LookAt(tmp_pos);
+            spawnedObject.transform.position = tmp_pos;
+        }
+
+
+
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+
+            objectsetFlag = true;
+        }
+    }
+}
+
