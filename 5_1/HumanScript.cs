ff976337e6f13266ac932762ff71f1c4d71a1e96
mirandora <info@mirandora.com>
2021-09-20 20:42:15 +0900

    Add files via upload

---
+++ 5_1/HumanScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanScript : MonoBehaviour
+{
+    LineRenderer radarLine;
+    Queue<Vector3> sphere_pos_queue = new Queue<Vector3>(); 
+    Vector3 sphere_pos;
+    Vector3 human_pos = new Vector3(0f, 1.85f, 0.0f);
+    Color32 ray_color = new Color32(220, 12, 12, 64);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		sphere_pos = GameObject.Find("SphereTarget").gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		sphere_pos = GameObject.Find("SphereTarget").gameObject.transform.position;
+		sphere_pos_queue.Enqueue(sphere_pos);
+
+		if (sphere_pos_queue.Count > 10){
+			Vector3 tmp_pos = sphere_pos_queue.Dequeue();			
+
+			Ray ray = new Ray(human_pos, tmp_pos-human_pos);
+			RaycastHit hit = new RaycastHit();
+			if(Physics.Raycast(ray ,out hit, 3)){
+				print("see!");
+			}
+			else{
+				print("not see!");
+			}
+			Debug.DrawRay(ray.origin, ray.direction*3, ray_color, 1.0f, false);
+		}
+    }
+}
---
+++ 5_1/SphereScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float random_move = Random.value;
+        if (random_move < 0.25f)
+        {
+            transform.Translate(0f, 0f, 0.1f);
+        }
+        else if ((0.25f <= random_move) && (random_move < 0.5f))
+        {
+            transform.Translate(0f, 0f, -0.1f);
+        }
+        else if ((0.5f <= random_move) && (random_move < 0.75f))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+        else
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        
+    }
+}
